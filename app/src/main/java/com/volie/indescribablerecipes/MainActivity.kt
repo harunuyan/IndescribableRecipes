2adf82a8fa360babaea8c3660acc63a96ea96130
harun <harunuyan6@gmail.com>
2023-05-15 16:13:16 +0300

    Added Navigation Graph

--- app/src/main/java/com/volie/indescribablerecipes/MainActivity.kt
+++ app/src/main/java/com/volie/indescribablerecipes/MainActivity.kt
@@ -2,10 +2,19 @@ package com.volie.indescribablerecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.volie.indescribablerecipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _mBinding: ActivityMainBinding? = null
+    private val mBinding get() = _mBinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
     }
 }
