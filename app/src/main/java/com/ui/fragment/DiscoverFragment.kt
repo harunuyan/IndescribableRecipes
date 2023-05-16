777ca40f5e16ef3b2693b48a86bad7d5decc862e
harun <harunuyan6@gmail.com>
2023-05-16 14:34:31 +0300

    Designed Discover Fragment

--- app/src/main/java/com/ui/fragment/DiscoverFragment.kt
+++ app/src/main/java/com/ui/fragment/DiscoverFragment.kt
@@ -1,8 +1,47 @@
 package com.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.volie.indescribablerecipes.R
+import com.volie.indescribablerecipes.databinding.BottomSheetLayoutFilterSearchBinding
+import com.volie.indescribablerecipes.databinding.FragmentDiscoverBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DiscoverFragment : Fragment() {
+    private var _mBinding: FragmentDiscoverBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentDiscoverBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.ivFilterSearch.setOnClickListener {
+            val bottomSheetDialog =
+                BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+            val bottomSheetView = LayoutInflater.from(requireContext())
+                .inflate(R.layout.bottom_sheet_layout_filter_search, mBinding.root, false)
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+
+            val mBindingBottomSheet = BottomSheetLayoutFilterSearchBinding.bind(bottomSheetView)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
 }
