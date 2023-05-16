ef805b308aa8f1ea9c19e5f53af288846a2686f3
harun <harunuyan6@gmail.com>
2023-05-16 17:12:19 +0300

    Designed Recipe Categories Fragment

---
+++ app/src/main/java/com/ui/fragment/RecipeCategoriesFragment.kt
@@ -0,0 +1,27 @@
+package com.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.volie.indescribablerecipes.databinding.FragmentRecipeCategoriesBinding
+
+class RecipeCategoriesFragment : Fragment() {
+    private var _mBinding: FragmentRecipeCategoriesBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        FragmentRecipeCategoriesBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
+}
