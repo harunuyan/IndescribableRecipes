972d7d57efef1f87d83c92a4a5a99c4b38d6620c
harun <harunuyan6@gmail.com>
2023-05-19 13:28:35 +0300

    Added Add Recipes Fragment & designed layout

--- app/src/main/java/com/ui/fragment/AddRecipesFragment.kt
+++ app/src/main/java/com/ui/fragment/AddRecipesFragment.kt
@@ -1,6 +1,27 @@
 package com.ui.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.volie.indescribablerecipes.databinding.FragmentAddRecipesBinding
 
-class AddRecipesFragment: Fragment() {
+class AddRecipesFragment : Fragment() {
+    private var _mBinding: FragmentAddRecipesBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentAddRecipesBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
 }
