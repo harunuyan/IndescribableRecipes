5bec95c4ec4128bed7b25b0d26082050072ac283
harun <harunuyan6@gmail.com>
2023-05-21 17:14:06 +0300

    Designed Profile View Pager fragments

--- app/src/main/java/com/ui/fragment/profile/ProfileAboutFragment.kt
+++ app/src/main/java/com/ui/fragment/profile/ProfileAboutFragment.kt
@@ -1,6 +1,27 @@
 package com.ui.fragment.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.volie.indescribablerecipes.databinding.FragmentProfileAboutBinding
 
 class ProfileAboutFragment : Fragment() {
+    private var _mBinding: FragmentProfileAboutBinding? = null
+    private val mBinding get() = _mBinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mBinding = FragmentProfileAboutBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mBinding = null
+    }
 }
--- app/src/main/java/com/ui/fragment/profile/ProfileRecipesFragment.kt
+++ app/src/main/java/com/ui/fragment/profile/ProfileRecipesFragment.kt
@@ -1,6 +1,27 @@
 package com.ui.fragment.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.volie.indescribablerecipes.databinding.FragmentProfileRecipesBinding
 
 class ProfileRecipesFragment : Fragment() {
+    private var _mbinding: FragmentProfileRecipesBinding? = null
+    private val mBinding get() = _mbinding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _mbinding = FragmentProfileRecipesBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _mbinding = null
+    }
 }
